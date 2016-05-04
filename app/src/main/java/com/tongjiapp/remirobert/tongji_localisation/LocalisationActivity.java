ef1569ec3fd7098f81ec88cf386f608d1b901f1a
rémi  <remirobert33530@gmail.com>
2016-05-04 20:33:43 +0800

    add new version

--- app/src/main/java/com/tongjiapp/remirobert/tongji_localisation/LocalisationActivity.java
+++ app/src/main/java/com/tongjiapp/remirobert/tongji_localisation/LocalisationActivity.java
@@ -1,72 +1,36 @@
 package com.tongjiapp.remirobert.tongji_localisation;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.tongjiapp.remirobert.tongji_localisation.LocationCollect.LocationManager;
 
 public class LocalisationActivity extends AppCompatActivity {
 
-    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 11;
-    private static final int PERMISSION_ACCESS_FINE_LOCATION = 10;
     private static final String TAG = "LocalisationActivity";
 
     private TextView mLocationTextView;
     private Button mButtonLocalisation;
+
+    private RecordDeviceManager mRecordDeviceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localisation);
 
-        mLocationTextView = (TextView)findViewById(R.id.location_text_view);
-        mButtonLocalisation = (Button)findViewById(R.id.button_get_localisation);
-        mLocationTextView.setText("localisation");
+        mLocationTextView = (TextView) findViewById(R.id.location_text_view);
+        mButtonLocalisation = (Button) findViewById(R.id.button_get_localisation);
+        mLocationTextView.setText("localisation 4");
 
-        mButtonLocalisation.setOnClickListener(new View.OnClickListener() {
+        mRecordDeviceManager = new RecordDeviceManager(this);
+        mRecordDeviceManager.newRecord(new RecordDeviceManagerListener() {
             @Override
-            public void onClick(View v) {
-                findLocalisation();
+            public void onRecordDeviceInformations(Record record) {
+                Log.v(TAG, "did record new device informations");
+                mLocationTextView.setText("battery : " + record.getDevice().getBatteryLevel());
             }
         });
-
-    }
-
-    private void findLocalisation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSION_ACCESS_FINE_LOCATION);
-        }
-        else {
-            getLocation();
-        }
-    }
-
-    private void getLocation() {
-        LocationManager.getInstance().getLocation(this);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_ACCESS_COARSE_LOCATION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // All good!
-                    getLocation();
-                    Toast.makeText(this, "Location COARSE OKAY", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Need your location! CARSE", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
     }
 }
